172d538534fa20dbaf3140f866fc9960fe6d73d6
Murat Erdoğan <stormwr@gmail.com>
2017-10-01 19:15:00 +0300

    Çalışiyi

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -12,11 +12,11 @@ namespace UnitTestProject1
             Assert.AreEqual(1, 1);
         }
 
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.AreEqual(1, 2);
-        }
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    Assert.AreEqual(1, 2);
+        //}
 
         [TestMethod]
         public void TestMethod2()
